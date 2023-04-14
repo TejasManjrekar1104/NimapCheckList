6982314305e481eafa21d3bd3167690e4b9b6378
Nimap <Nimap@DESKTOP-RRJ6NCT>
2023-04-14 18:19:01 +0530

    14-04-2023

---
+++ GenericClass/GenericClass/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericClass
+{
+    class Generics<T>
+    {
+        T box;
+
+        public Generics(T a)
+        {
+            this.box = a;
+        }
+
+        public T GetBox()
+        {
+            return this.box;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Generics<int> g = new Generics<int>(10);
+            Generics<string> s = new Generics<string>("Tejas");
+
+            Console.WriteLine(g.GetBox());
+            Console.WriteLine(s.GetBox());
+
+        }
+    }
+}
---
+++ GenericMethod/GenericMethod/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMethod
+{
+    class Arrayy
+    {
+        public static void ShowArray<T>(T[] arr)
+        {
+            for(int i = 0; i<arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Number = {10,20,30,40,50,60,70,80,90 };
+
+            string[] Name = { "Tejas", "Manjrekar" };
+
+            double[] Num = {1.1,2.3,3.4,4.5,5.6,6.7 };
+
+            Arrayy.ShowArray(Number);
+            Arrayy.ShowArray(Name);
+            Arrayy.ShowArray(Num);
+
+        }
+    }
+}
