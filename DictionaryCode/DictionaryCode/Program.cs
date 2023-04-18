92074fb0bc6634c3139778ae08968785ffb477f5
Nimap <Nimap@DESKTOP-RRJ6NCT>
2023-04-18 19:36:25 +0530

    18/04/2023

---
+++ DictionaryCode/DictionaryCode/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> d = new Dictionary<int, string>();
+            d.Add(1, "Tej");
+            d.Add(0, "Nimap");
+            d.Add(5, "Te");
+            d.Add(3, null);
+            d.Add(6, null);
+            d.Add(1, "Tej");
+
+
+            foreach (KeyValuePair<int,string> print in d)
+            {
+                Console.WriteLine(print);
+            }
+        }
+    }
+}
---
+++ ListCode/ListCode/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Num = new List<int>();
+            Num.Add(10);
+            Num.Add(15);
+            Num.Add(20);
+            Num.Add(25);
+            Num.Add(30);
+            Num.Add(35);
+
+            foreach(int n in Num)
+            {
+                Console.WriteLine(n);
+            }
+
+            Console.WriteLine("_______________________________");
+
+            List<string> Name = new List<string>();
+            Name.Add("Tejas");
+            Name.Add("Manjrekar");
+
+            foreach(string n in Name)
+            {
+                Console.WriteLine(n);
+            }
+
+            Console.WriteLine("_______________________________");
+
+            List<double> Num2 = new List<double>();
+            Num2.Add(10.5);
+            Num2.Add(13.5);
+            Num2.Add(16.5);
+            Num2.Add(19.5);
+            Num2.Add(20.5);
+            foreach (double n in Num2)
+            {
+                Console.WriteLine(n);
+            }
+            Console.WriteLine("_______________________________");
+
+            Num2.Insert(2, 19.2);
+            foreach (double n in Num2)
+            {
+                Console.WriteLine(n);
+            }
+
+            Console.WriteLine("--------------------------------");
+
+            Num.Reverse();
+            foreach (int n in Num)
+            {
+                Console.WriteLine(n);
+            }
+
+        }
+    }
+}
