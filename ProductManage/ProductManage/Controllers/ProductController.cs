04dd382cc44052156f638c29812547ab591d0cf3
Nimap <Nimap@DESKTOP-RRJ6NCT>
2023-05-02 19:06:37 +0530

    Curd Task

---
+++ ProductManage/ProductManage/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using ProductManage.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductManage.Controllers
+{
+    public class ProductController : Controller
+    {
+        MyDbContext db = new MyDbContext();
+
+        // GET: Product
+        public async Task<ActionResult> ProductList()
+        {
+            var data = db.Categories.ToListAsync();
+            return View(data);
+        }
+    }
+}
